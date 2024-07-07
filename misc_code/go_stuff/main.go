243475c9c1cf638f892034bd46a2861bc04ab03d
dmgedgoods <smcdani8@mail.ccsf.edu>
2024-07-06 20:31:24 -0700

    add misc python and other random code practive stuff. Need to organize

---
+++ misc_code/go_stuff/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "github.com/rivo/tview"
+    "github.com/gdamore/tcell/v2"
+)
+
+func main() {
+    // Create a new primitive which is a full-screen application.
+    app := tview.NewApplication()
+
+    // Create a new text view and set some text to display.
+    textView := tview.NewTextView().
+        SetText("Hello, TUI world! Press 'q' to quit.")
+
+    // Set up input capture to quit the application when 'q' is pressed.
+    textView.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+        if event.Key() == tcell.KeyRune && event.Rune() == 'q' {
+            app.Stop()
+        }
+        return event
+    })
+
+    // Set the root of the application to the text view and run the application.
+    if err := app.SetRoot(textView, true).Run(); err != nil {
+        panic(err)
+    }
+}
+
--- python/scratch.py
+++ python/scratch.py
@@ -1,11 +1,32 @@
-#name = 'Mary'
-#password = 'swordfish'
-name = input()
-password = input()
+import socket
+import os
 
-if name == 'Mary':
-    print('Hello, Mary')
-    if password == 'swordfish':
-        print('Access Granted')
+HOST = '0.0.0.0'  # Adjust as necessary
+
+def main():
+    # Create a raw socket, bind to public interface
+    if os.name == 'nt':
+        socket_protocol = socket.IPPROTO_IP
     else:
-        print('Wrong password')
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+    sniffer.bind((HOST, 0))
+
+    # Include the IP header in the capture
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    # Enable promiscuous mode on Windows
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    # Read one packet
+    print(sniffer.recvfrom(65565))
+
+    # If on Windows, turn off promiscuous mode
+    if os.name == 'nt':
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+if __name__ == '__main__':
+    main()
+
