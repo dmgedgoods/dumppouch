429755d1c48e6a90045c49dab3518d9f6cdc2ece
dmgedgoods <smcdani8@mail.ccsf.edu>
2024-07-09 21:14:11 -0700

    jupyter nb update

--- python/python/scratch.py
+++ python/python/scratch.py
@@ -1,11 +1,8 @@
-#name = 'Mary'
-#password = 'swordfish'
-name = input()
-password = input()
+name = str(input())
 
-if name == 'Mary':
-    print('Hello, Mary')
-    if password == 'swordfish':
-        print('Access Granted')
-    else:
-        print('Wrong password')
+if name == 'mack':
+    print('Hello mack.')
+else:
+    break
+
+password = int(input())
