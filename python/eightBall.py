f73e114db2c0bdbcf717b24b6ac0c2449292933e
dmgedgoods <smcdani8@mail.ccsf.edu>
2024-07-10 21:21:38 -0700

    ATBS update

---
+++ python/eightBall.py
@@ -0,0 +1,7 @@
+import random 
+messages = ['Unknown',
+            'Eat shit',
+            'Maybe later',
+            'Reply hazy, try again',
+            'Not looking good, buddy']
+print(messages[random.randint(0, len(messages) - 1)])
