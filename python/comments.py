b6776aec0462e49dd980fbd681c1bad615d48d01
dmgedgoods <smcdani8@mail.ccsf.edu>
2024-07-24 22:00:00 -0700

    python notebook ATBS

--- python/comments.py
+++ python/comments.py
@@ -4,9 +4,9 @@ do what you want"""
 name = input('What is your name?\n')
 age = input('How old are you?\n')
 
-print(f'My name is {name} and I am {age} years old')
+answer = print(f'My name is {name} and I am {age} years old')
 
 name = name.upper()
 print(f'My name is {name} and I am {age} years old')
-
-
+print(name.split('a'))
+print(answer)
